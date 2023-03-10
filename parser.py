b662dd50db0424f932da345c98783d9165e80d30
Andrey Vlasenko <andrey.vlasenko@skalex.io>
2023-03-10 21:10:35 +0200

    minor changes in work with json in parser

--- parser.py
+++ parser.py
@@ -3,26 +3,27 @@ from bs4 import BeautifulSoup as bs
 import json
 
 
-def into_tuple(value):
-    return tuple(i for i in value)
-
-
 has_more = True
 skip_value = 0
 while has_more:
     url = f'https://uahelpers.com/api/volunteers/search?location=&category=&skip={skip_value}'
     response = requests.request("GET", url)
+
     parsed_text = bs(response.text, 'lxml')
     full_json_str = json.loads(parsed_text.find('p').text)
+
     has_more = full_json_str['hasMore']
     result = full_json_str['result']
-    result_json = json.dumps(result[0])
-    dict_json = json.loads(result_json)
-    for tag in dict_json:
-        values = dict_json[tag]
-        if type(values) == list:
-            tuple_values = into_tuple(values)
-            print(tuple_values.__getitem__(0))
-        else:
-            print(values)
+
+    for proposition in result:
+        proposition_json = json.dumps(proposition)
+        dict_json = json.loads(proposition_json)
+        for tag in dict_json:
+            values = dict_json[tag]
+            if type(values) == list:
+                print(tag, ", ".join(values))
+            else:
+                print(tag, values)
+        print()
+
     skip_value += 9
