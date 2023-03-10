ad0f78520bb3d8c41f8b212de7eccac294096c09
EternityRei <anastasiamayorova2003@gmail.com>
2023-03-10 17:30:55 +0200

    Output all offers without tags

--- parser.py
+++ parser.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup as bs
 import json
 
+
+def into_tuple(value):
+    return tuple(i for i in value)
+
+
 has_more = True
 skip_value = 0
 while has_more:
@@ -10,5 +15,14 @@ while has_more:
     parsed_text = bs(response.text, 'lxml')
     full_json_str = json.loads(parsed_text.find('p').text)
     has_more = full_json_str['hasMore']
+    result = full_json_str['result']
+    result_json = json.dumps(result[0])  # THIS
+    dict_json = json.loads(result_json)
+    for tag in dict_json:  # THIS
+        values = dict_json[tag]
+        if type(values) == list:
+            tuple_values = into_tuple(values)
+            print(tuple_values.__getitem__(0))
+        else:
+            print(values)
     skip_value += 9
-    print(parsed_text.prettify())
