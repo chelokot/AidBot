55da59ef0994bc768d9fcf95610dc0bacfd5ebec
Andrey Vlasenko <andrey.vlasenko@skalex.io>
2023-04-14 02:56:03 +0300

    this class is abstract

--- src/database/data types/ProposalRequest.py
+++ src/database/data types/ProposalRequest.py
@@ -7,14 +7,14 @@
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-from typing import List, Generic, TypeVar, Dict, Optional
+from typing import Generic, TypeVar, Dict, Optional
 from embeddings.Embedding import Embedding
 from embeddings.TextEmbedder import TextEmbedder
 from abc import ABC, abstractmethod, abstractproperty
 
 EmbedType = TypeVar('EmbedType', bound=Embedding)
 
-class ProposalRequest(Generic[EmbedType]):
+class ProposalRequest(Generic[EmbedType], ABC):
     @abstractmethod
     def __init__(self, characteristics: Dict[str, str], embedder: TextEmbedder[EmbedType]):
         pass
