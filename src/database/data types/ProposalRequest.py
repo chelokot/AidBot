53e66943bad3dbc8404eaddcf4388f01dbef41c5
Andrey Vlasenko <andrey.vlasenko@skalex.io>
2023-04-14 02:53:37 +0300

    It's better to get None than runtime error here

--- src/database/data types/ProposalRequest.py
+++ src/database/data types/ProposalRequest.py
@@ -7,7 +7,7 @@
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-from typing import List, Generic, TypeVar, Dict
+from typing import List, Generic, TypeVar, Dict, Optional
 from embeddings.Embedding import Embedding
 from embeddings.TextEmbedder import TextEmbedder
 from abc import ABC, abstractmethod, abstractproperty
@@ -24,7 +24,7 @@ class ProposalRequest(Generic[EmbedType]):
         pass
 
     @abstractmethod
-    def get_characteristic(self, characteristic: str) -> str:
+    def get_characteristic(self, characteristic: str) -> Optional[str]:
         pass
 
     @abstractmethod
